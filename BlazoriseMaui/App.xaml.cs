fdc6ecca606b3b9712c85b6e76c3d5e50152c4d5
Emanuel Paul <emanuel.paul@hotmail.com>
2022-09-29 18:58:09 +0300

    add blazor maui starter project

---
+++ BlazoriseMaui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazoriseMaui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ BlazoriseMaui/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazoriseMaui;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
