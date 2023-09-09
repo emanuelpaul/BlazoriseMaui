246a3a66a29e03db899e920f0a096a4c6d87936e
Emanuel Paul <emanuel.paul@hotmail.com>
2023-09-09 18:51:13 +0300

    test button on android

--- BlazoriseMaui/BlazoriseWorkaround/WorkaroundIComponentDisposer.cs
+++ BlazoriseMaui/BlazoriseWorkaround/WorkaroundIComponentDisposer.cs
@@ -2,7 +2,7 @@ using Blazorise;
 
 namespace BlazoriseMaui.BlazoriseWorkaround;
 
-internal class WorkaroundIComponentDisposer: IComponentDisposer
+internal class WorkaroundIComponentDisposer : IComponentDisposer
 {
 	void IComponentDisposer.Dispose<TComponent>(TComponent component)
 	{
